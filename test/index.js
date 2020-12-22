639102e04b8c37839bf01ac70104ea162916cbe4
PHDeveloper <phbm@hotmail.com>
2020-12-22 02:04:27 -0300

    Corrigindo o teste

--- test/index.js
+++ test/index.js
@@ -6,7 +6,7 @@ const somarNumeros = (a,b) => a + b
 describe('Somas', () =>{
     it('Soma de dois numeros | 2 e 3', (done) => {
         const resultado = somarNumeros(2,3)
-        expect(resultado).be.equal(1)
+        expect(resultado).be.equal(5)
         done()
     })
 
